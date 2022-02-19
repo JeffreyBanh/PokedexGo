dcf8e4e09fb7e2a7eb053e9c10b7f385523a4566
Corny Mac <kevin130366@gmail.com>
2022-02-19 02:44:22 -0700

    test

---
+++ src/graphql/get-pokemon.js
@@ -0,0 +1,20 @@
+// import gql from 'graphql-tag';
+
+// export const GET_POKEMONS = gql`
+//     query pokemons($first: Int!) {
+//         pokemon(first: $first) {
+//             id
+//             name
+//             image
+//             maxHP
+//             maxCP
+//             attacks {
+//                 special {
+//                     name
+//                     damage
+//                 }
+//             }
+
+//         }
+//     }
+// `
